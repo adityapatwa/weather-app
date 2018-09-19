68a1254f94e1c375b5e59a79b8295c8adc4bf73a
Aditya Patwa <adityapatwa@live.com>
2018-09-19 14:22:02 -0400

    Modified the code to accept user provided address to fetch the latitude and longitude for the same

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        address : {
+            demand: true,
+            describe: "Fetch weather for the address",
+            alias: "a",
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=09jiKAxod7j3yPUWJxUZDwG2503Z4sJJ&location=1301%20lombard%20street%20philadelphia',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=09jiKAxod7j3yPUWJxUZDwG2503Z4sJJ&location=${encodeURI(argv.address)}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].providedLocation.location}`);
