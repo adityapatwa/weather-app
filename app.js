8a2932d5a6a02a722229dd5bd0889c642d9bdcb5
Aditya Patwa <adityapatwa@live.com>
2018-09-19 14:34:17 -0400

    Modified the code to handle error conditions which might occur while fetching the coordinates for the provided location

--- app.js
+++ app.js
@@ -15,11 +15,28 @@ const argv = yargs
     .argv;
 
 
+const url = 'http://www.mapquestapi.com/geocoding/v1/address?';
+const key = '09jiKAxod7j3yPUWJxUZDwG2503Z4sJJ';
+const address = encodeURIComponent(argv.address);
+const urlStr = `${url}key=${key}&location=${address}`;
+
 request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=09jiKAxod7j3yPUWJxUZDwG2503Z4sJJ&location=${encodeURI(argv.address)}`,
+    url: urlStr,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].displayLatLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].displayLatLng.lng}`);
+    if (error) {
+        console.log('Unable to connect to mapquest.api')
+    } else {
+        try {
+            if (body.results[0].locations[0].street === '') {
+                console.log('No Result Found')
+            } else {
+                console.log(`Address: ${body.results[0].locations[0].street}`);
+                console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+                console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+            }
+        } catch (e) {
+            console.log(JSON.stringify(body, undefined, 2))
+        }
+    }
 });
